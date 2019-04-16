67f6efa2ccd82bebe3e5f295c4648d2de9c48a9c
VtoCorleone <shane.chapman@live.com>
2019-04-15 19:32:30 -0700

    Added type check for num

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function Incrementor() { };
+function Incrementor(type = 'num') {
+  this.type = type;
+};
 
 Incrementor.prototype.increment = function increment(value) {
-  return value += 1;
+  if (this.type === 'num') {
+    return value += 1;
+  }
+
+  return new Error(`Unsupported type: ${this.type}`);
 }
 
 module.exports = Incrementor;
--- index.test.js
+++ index.test.js
@@ -3,9 +3,15 @@ const assert = require('assert');
 const Incrementor = require('./index');
 
 describe('Incrementor', () => {
-  test('should increment value by 1', () => {
-    const incrementor = new Incrementor();
+  test('should increment value by 1 when type is \'num\'', () => {
+    const incrementor = new Incrementor('num');
     const value = incrementor.increment(3);
     expect(value).toBe(4);
+  });
+
+  test('should return error when type is not \'num\'', () => {
+    const incrementor = new Incrementor('alpha');
+    const value = incrementor.increment(3);
+    expect(value).toEqual(new Error(`Unsupported type: alpha`));
   });
 });
