053a1efcf870e69fc44fb1cbca383e7be96bb9ba
VtoCorleone <shane.chapman@live.com>
2019-04-16 06:32:34 -0700

    Added NUMERIC type
    - added eslint

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
-function Incrementor({ type = 'integer' }) {
-  if (type !== 'integer') {
+
+const incrementorType = {
+  INTEGER: 'integer',
+  NUMERIC: 'numeric',
+};
+
+function Incrementor({
+  type = incrementorType.INTEGER,
+  leftPadding = 0,
+}) {
+  if (type !== incrementorType.INTEGER || type !== incrementorType.NUMERIC) {
     this.error = new Error(`Invalid type: ${type}`);
   } else {
     this.error = null;
   }
 
   this.type = type;
-};
+  this.leftPadding = leftPadding;
+}
 
 Incrementor.prototype.increment = function increment(value) {
-  if (this.type === 'integer') {
+  if (this.type === incrementorType.INTEGER) {
     return {
-      value: value += 1,
+      value: value + 1,
       error: null,
     };
+  }
+
+  if (this.type === incrementorType.NUMERIC) {
+    try {
+      let num = Number.parseInt(value, 10);
+      const incremented = `${num += 1}`;
+
+      if (this.leftPadding > 0) {
+        if (incremented.length < this.leftPadding) {
+          const diff = this.leftPadding - incremented.length + 1;
+          return {
+            value: incremented.padStart(diff, '0'),
+            error: null,
+          };
+        }
+
+        return {
+          value: incremented,
+          error: null,
+        };
+      }
+
+      return {
+        value: incremented,
+        error: null,
+      };
+    } catch (error) {
+      return {
+        value: null,
+        error,
+      };
+    }
   }
 
   return {
     value: null,
     error: new Error(`Unsupported type: ${this.type}`),
   };
-}
+};
 
 module.exports = Incrementor;
--- index.test.js
+++ index.test.js
@@ -1,17 +1,22 @@
-const assert = require('assert');
-
 const Incrementor = require('./index');
 
 describe('Incrementor', () => {
+  test('should return error when type is not a valid type', () => {
+    const incrementor = new Incrementor({ type: 'alpha' });
+    const { error } = incrementor.increment(3);
+    expect(error).toEqual(new Error('Unsupported type: alpha'));
+  });
+
   test('should increment value by 1 when type is \'integer\'', () => {
     const incrementor = new Incrementor({ type: 'integer' });
     const { value } = incrementor.increment(3);
     expect(value).toBe(4);
   });
 
-  test('should return error when type is not \'integer\'', () => {
-    const incrementor = new Incrementor({ type: 'alpha' });
-    const { error } = incrementor.increment(3);
-    expect(error).toEqual(new Error(`Unsupported type: alpha`));
+  test('should increment value by 1 when type is \'numeric\'', () => {
+    const incrementor = new Incrementor({ type: 'numeric', leftPadding: 4 });
+    const { error, value } = incrementor.increment(3);
+    expect(error).toBeNull();
+    expect(value).toBe('0004');
   });
 });
